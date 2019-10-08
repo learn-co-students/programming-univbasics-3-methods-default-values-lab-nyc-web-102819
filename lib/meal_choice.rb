2c87a9053456841693b9cd22f6da55341aa21edc
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-08 02:57:11 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,9 +1,11 @@
 # Your code here
-def 
-  protein = meat
-  veg2 = macaroni
-  veg1 = broccoli 
+def meal_choice(veg1, veg2, protein ="meat")
   
     puts "What a nutritious meal!"
-    puts "A plate of #{protein} with #{veg1} and #{veg2}."
+    meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+    puts meal
+    return meal # return here is optional. Ruby methods automatically return the last expression
+    
 end
+
+
